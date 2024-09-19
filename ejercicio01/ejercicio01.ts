2f3c0c907c24a2078c45e9c0a9bfad8e60e4613f
ManuFoulkes <manuel.foulkes90@gmail.com>
2024-09-19 18:00:11 -0300

    agrega ejercicio01

---
+++ ejercicio01/ejercicio01.ts
@@ -0,0 +1,48 @@
+import * as rs from "readline-sync";
+
+let cantParticipantes: number;
+let sabor: number;
+let presentacion: number;
+let dificultad: number;
+
+const pedirCantParticipantes = (): number => rs.questionInt(`Ingrese la cantidad de participantes: `);
+
+const pedirPuntaje = (tipo: String, participante: number): number => {
+    return rs.questionInt(`Ingrese el puntaje para ${tipo} de la torta del participante numero ${participante}: \n`);
+} 
+
+const calcularPuntaje = (sabor: number, presentacion: number, dificultad: number): number => {
+    return sabor + presentacion + dificultad;
+}
+
+const determinarGanador = (): void => {
+
+    cantParticipantes = pedirCantParticipantes();
+    let mayorPuntaje: number = 0;
+    let ganador: number = -1;
+    let empates: number = 0;
+
+    for(let i = 0; i < cantParticipantes; i++) {
+        sabor = pedirPuntaje("sabor", i + 1);
+        presentacion = pedirPuntaje("presentacion", i + 1);
+        dificultad = pedirPuntaje("dificultad", i + 1);
+
+        const puntaje = calcularPuntaje(sabor, presentacion, dificultad);
+
+        if(puntaje > mayorPuntaje) {
+            mayorPuntaje = puntaje;
+            ganador = i + 1;
+            empates = 0;
+        } else if(puntaje === mayorPuntaje) {
+            empates++;
+        }
+    }
+
+    if(ganador != -1 && empates > 0) {
+        console.log(`Se registro ${empates} empate/s con un puntaje maximo de ${mayorPuntaje}`);
+    } else if(ganador != -1) {
+        console.log(`El ganador fue el participante numero ${ganador} con un puntaje de ${mayorPuntaje}.`);
+    }
+}
+
+determinarGanador();
